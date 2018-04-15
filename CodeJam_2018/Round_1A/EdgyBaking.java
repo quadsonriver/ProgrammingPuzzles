cc0e3b2bbc62794585706f35ef0a2219c12acca7
Slothonaut <zranck25@eagles.ewu.edu>
2018-04-15 13:17:12 -0700

    Adding 1A question placeholder

---
+++ CodeJam_2018/Round_1A/EdgyBaking.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+
+public class EdgyBaking 
+{
+  static Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+  public static void main(String[] args) throws InterruptedException 
+  {
+    int t = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
+
+    
+    
+    for (int i = 1; i <= t; ++i) 
+    {    	
+    
+    	//todo
+		System.out.println("Case #" + (i) + ": ");
+    }
+  }
+  
+
+}
